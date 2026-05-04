055df7b821a3f0917141b17d4d54166c7aa85316
Isak Stene-Pettersen <Isakstenep@gmail.com>
2026-05-04 12:53:04 +0200

    Oppsett av Nextjs
    Installert Nextjs, og gjort klar for start av utvikling av prosjektet

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+export const metadata: Metadata = {
+    title: 'Fagprøve',
+    description: 'Fagprøve Isak Stene-Pettersen',
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" className={`h-full antialiased`}>
+            <body className="min-h-full flex flex-col">{children}</body>
+        </html>
+    );
+}
